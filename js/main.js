f4bb18e4650f53ad2881f7befe0e4b88e790defe
SmallGitLearner <45071001+SmallGitLearner@users.noreply.github.com>
2019-05-18 22:30:20 +0800

    first commit
    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by chen on 2019/4/10.
+ */
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
+    });
+});
